17f93ceb758e1fd9f576cbc1fef67abd799e84c4
Nickothan <s1nicolasrg@gmail.com>
2022-01-18 21:50:24 -0500

    Make Footer component.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import Hero from "../../components/Hero"
 import Features from "../../components/Features"
 import Testimonials from "../../components/Testimonials"
 import Price from "../../components/Price"
+import Footer from "../../components/Footer"
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Features />
       <Testimonials />
       <Price />
+      <Footer />
     </HomeContainer>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+//* Import styles
+import { FooterContainer } from "./styles"
+
+//* Import assets
+import TwitterBlack from "../Assets/TwitterBlack.svg"
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <div>
+        <h3>Chirp.</h3>
+
+        <ul>
+          <img src={TwitterBlack} alt="Twitter black" />{" "}
+          <li>
+            <a href="">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="">Terms of Use</a>
+          </li>
+        </ul>
+      </div>
+    </FooterContainer>
+  )
+}
